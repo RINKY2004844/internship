f955625dc6f946675a87bd12cff31d3bb3a952a4
RINKY2004844 <145477512+RINKY2004844@users.noreply.github.com>
2024-07-26 20:03:53 +0530

    Update day2.js

--- day2.js
+++ day2.js
@@ -8,3 +8,44 @@ console.log("area of the traingle is : ", area)
 let c=20;
 let f=((9/5)*c)+32;
 console.log("temperature in Fahrenheit is : ",f)
+
+// JavaScript Program to Find the Factorial of a Number
+let a=5;
+let fact=1;
+for (let i =1; i <=a; i++ ){
+    fact*=i;    
+}
+console.log(fact);
+
+/*JavaScript Program to function as Calculator
+‘+’ : sum
+‘-’  : subtract
+‘x’  : multiplication
+‘/’   : division
+‘%’ : display remainder*/
+let char="+";
+let d=10;
+let e=5;
+
+switch(char){
+    case "+":
+        console.log(d+e);
+        break;
+    case "-":
+        console.log(d-e);
+        break;
+    case"*":
+        console.log(d*e);
+        break;
+    case"/":
+        console.log(d/e);
+    case"%":
+        console.log(d%e);
+    default:
+        console.log("wrong character")
+    
+}
+
+
+
+
